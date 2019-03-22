49ab8ea6c750bc236bddcbea181c09dfa27e6ce3
Mohammed Aman Uddin Haseeb <48690505+mhaseeb11@users.noreply.github.com>
2019-03-21 21:49:32 -0500

    Initial commit

---
+++ lombok-master/src/main/java/com/Lombok/Employee.java
@@ -0,0 +1,20 @@
+package com.Lombok;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    private String firstname;
+    private String middlename;
+    private String lastname;
+    private String emailid;
+    private Date Birthday;
+    private int duration;
+    private int age;
+  
+	
+
+}
---
+++ lombok-master/src/main/java/com/Lombok/Lombok_Demo.java
@@ -0,0 +1,13 @@
+package com.Lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lombok_Demo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lombok_Demo.class, args);
+		
+	}
+}
